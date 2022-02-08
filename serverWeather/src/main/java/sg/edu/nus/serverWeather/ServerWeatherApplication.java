a5672d970da444e0eaa69519f84beabcb2adc948
Bonny <JupiterSignature@live.com>
2022-02-08 13:06:48 +0800

    Initial Commit

---
+++ serverWeather/src/main/java/sg/edu/nus/serverWeather/ServerWeatherApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.serverWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerWeatherApplication.class, args);
+	}
+
+}
---
+++ serverWeather/src/test/java/sg/edu/nus/serverWeather/ServerWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.serverWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
